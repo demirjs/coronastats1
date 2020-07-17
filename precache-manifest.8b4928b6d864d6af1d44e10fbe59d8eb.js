d1c8bc88b5fdc6a624faa143ae3eac2697e696e8
demiryuca <demiryuca218@gmail.com>
2020-07-17 20:07:53 +0300

    Updates

---
+++ precache-manifest.8b4928b6d864d6af1d44e10fbe59d8eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c597bcdeeb798b46398370406862e2de",
+    "url": "/coronastats1/index.html"
+  },
+  {
+    "revision": "95cfbd346af743ce9cf2",
+    "url": "/coronastats1/static/css/main.7634e756.chunk.css"
+  },
+  {
+    "revision": "6563156c7a4910275a7f",
+    "url": "/coronastats1/static/js/2.bace30d5.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/coronastats1/static/js/2.bace30d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95cfbd346af743ce9cf2",
+    "url": "/coronastats1/static/js/main.d98e583e.chunk.js"
+  },
+  {
+    "revision": "9d249f9ed5470b3054fc",
+    "url": "/coronastats1/static/js/runtime-main.d7d1f6ab.js"
+  },
+  {
+    "revision": "7786bbd41b0ab3b8fdc2d472789e306e",
+    "url": "/coronastats1/static/media/COVID-19.bf2a287a.7786bbd4.png"
+  }
+]);
